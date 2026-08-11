fd06ec8b64e0faf08d00bb156440fd0583965676
shhyou <shu-hung.you@eecs.northwestern.edu>
2026-08-11 18:55:51 +0800

    Import test configuration from drracket-test

---
+++ htdp-drracket-test/tests/htdp-lang/info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define test-timeouts '(("test-engine-test.rkt" 300)))
+
+(define test-responsibles '(("test-engine-test.rkt" sperber)
+                            ("language-test.rkt" (robby matthias))
+                            (all robby)))
